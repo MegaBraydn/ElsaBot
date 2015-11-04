580ae1af8d99b5f4de77bdfa06eef13734e02f32
MegaBraydn <Braydnl@gmail.com>
2015-11-03 22:48:59 -0800

    Update dubtrack.js

--- dubtrack.js
+++ dubtrack.js
@@ -2,11 +2,13 @@ var heartNo = [":heart: :yellow_heart: :green_heart: :blue_heart: :purple_heart:
 var i = 0;
 function startSpamDialouge() {
     if (confirm("Taste the Rainbow?") === true) {
-      var myVar;
-      function myFunction() {
-        myVar = setInterval(spam, 300);
-      }
-      myFunction();
+        var myVar;
+        function myFunction() {
+            myVar = setInterval(spam, 300);
+        }
+        $('#chat-txt-message').val("http://40.media.tumblr.com/PFO2xF03Amxr6pp8vQCxGHero1_500.jpg");
+        Dubtrack.room.chat.sendMessage();
+        myFunction();
     } 
     else {
       alert("good work");
