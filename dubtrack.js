d1f088f3f7e9834e930d749529d44e9d5716ffe0
MegaBraydn <Braydnl@gmail.com>
2015-11-03 17:07:24 -0800

    Update dubtrack.js

--- dubtrack.js
+++ dubtrack.js
@@ -1,3 +1,12 @@
 $('#chat-txt-message').val(':heart: :yellow_heart: :green_heart: :blue_heart: :purple_heart: :heart: :yellow_heart: :green_heart: :blue_heart: :purple_heart:');
 Dubtrack.room.chat.sendMessage();
+$('#chat-txt-message').val(':yellow_heart: :green_heart: :blue_heart: :purple_heart: :heart: :yellow_heart: :green_heart: :blue_heart: :purple_heart: :heart:');
+Dubtrack.room.chat.sendMessage();
+$('#chat-txt-message').val(':green_heart: :blue_heart: :purple_heart: :heart: :yellow_heart: :green_heart: :blue_heart: :purple_heart: :heart: :yellow_heart:');
+Dubtrack.room.chat.sendMessage();
+$('#chat-txt-message').val(':blue_heart: :purple_heart: :heart: :yellow_heart: :green_heart: :blue_heart: :purple_heart: :heart: :yellow_heart: :green_heart:');
+Dubtrack.room.chat.sendMessage();
+$('#chat-txt-message').val(':purple_heart: :heart: :yellow_heart: :green_heart: :blue_heart: :purple_heart: :heart: :yellow_heart: :green_heart: :blue_heart:');
+Dubtrack.room.chat.sendMessage();
+
 
