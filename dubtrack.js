0cab8bb89b16fb0bb44645556c79511b9ce44d1f
MegaBraydn <Braydnl@gmail.com>
2015-11-03 16:25:56 -0800

    Update dubtrack.js

--- dubtrack.js
+++ dubtrack.js
@@ -1,5 +1,7 @@
 var elem = document.getElementById("chat-txt-message");
 elem.value = ":heart: :yellow_heart: :green_heart: :blue_heart: :purple_heart: :heart: :yellow_heart: :green_heart: :blue_heart: :purple_heart:";
-var e = $.Event( "keypress", { which: 13 } );
-document.getElementById("chat-txt-message").trigger(e);
+var e = jQuery.Event("keypress");
+e.which = 13; //choose the one you want
+e.keyCode = 13;
+$("#chat-txt-message").trigger(e);
 
